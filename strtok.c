d1ddc4bb3a17cc84dbd6c1f9a5d543fa50cda0b0
Vicky041 <osemekev22@gmail.com>
2023-08-19 14:31:09 -0700

    strtok tutorial

---
+++ strtok.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void)
+{
+char *src = "jesus loves you";
+char *str = malloc(sizeof(char) * strlen(src));
+char *delim = " ";
+char *token;
+
+strcpy(str, src);
+token = strtok(str, delim);
+
+while(token)
+{
+printf("%s\n", token);
+token = strtok(NULL, delim);
+}
+return 0;
+}
